ae6ed398510871171c2c95a56494f3a0fdbc604d
Hector Leal <136043873+no-c-123@users.noreply.github.com>
2025-11-21 16:06:38 -0600

    feat: add Authorization and x-api-key to allowed CORS headers

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin || '*');
   }
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Stripe-Signature, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Stripe-Signature, X-Requested-With, Authorization, x-api-key');
   res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
